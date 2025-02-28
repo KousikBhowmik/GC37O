0f655121439d252195f65c538233febdb5f0e5c7
Kousik Bhowmik <kousikbhowmik888@gmail.com>
2025-02-28 23:50:50 +0530

    test folder deleted

--- test/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({
-  server: {
-    port: 5174,
-    open: true,
-  },
-  plugins: [tailwindcss()],
-});
