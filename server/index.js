41647e3c6750cf2180982809a0440085896eb900
Kousik Bhowmik <kousikbhowmik888@gmail.com>
2025-03-03 01:54:47 +0530

    cors origin fix

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     // @ts-ignore
-    origin: [process.env.CLIENT_URL],
+    // origin: [process.env.CLIENT_URL],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
